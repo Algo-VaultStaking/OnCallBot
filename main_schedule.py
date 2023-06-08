dbf9ba5d3393b27fac3d660295eab1506d11dcca
algo-vaultstaking <vaultstaking@gmail.com>
2023-06-07 20:28:53 -0400

    Init

--- main_schedule.py
+++ main_schedule.py
@@ -7,7 +7,6 @@ import discord
 import database
 
 from discord.ext import commands, tasks
-from discord.utils import get
 
 # Load config
 c = configparser.ConfigParser()
@@ -37,12 +36,12 @@ async def check_schedule():
     db_connection = database.get_db_connection()
 
     await bot.wait_until_ready()
-    current_date = "tue"#datetime.datetime.now(pytz.timezone('UTC')).strftime('%a')
-    current_time = "0407"#datetime.datetime.now(pytz.timezone('UTC')).strftime('%H%M')
+    current_date = datetime.datetime.now(pytz.timezone('UTC')).strftime('%a')
+    current_time = datetime.datetime.now(pytz.timezone('UTC')).strftime('%H%M')
     print(current_date, current_time)
 
     # results = (start, end, user, guild)
-    schedules = [('0407', '0408', '<@712863455467667526>', 837853470136467517)]#database.get_scheduled_users_by_datetime(db_connection, current_date, current_time)
+    schedules = database.get_scheduled_users_by_datetime(db_connection, current_date, current_time)
 
     for schedule in schedules:
         start = str(schedule[0])
@@ -53,14 +52,13 @@ async def check_schedule():
         if start == current_time:
             guild = bot.get_guild(guild_id)
             member = guild.get_member(member_id)
-            print(guild.members)
             role = guild.get_role(1116012502879305749)
             await member.add_roles(role)
 
         if end == current_time:
             guild = bot.get_guild(guild_id)
             member = guild.get_member(member_id)
-            role = get(member.server.roles, name="Bots")
+            role = guild.get_role(1116012502879305749)
             await member.remove_roles(role)
 
     db_connection.close()
